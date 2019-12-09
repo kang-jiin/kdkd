29b94858c4b5e3f5bed290ac8436a8431200e0d8
kangG13 <dmssk13@gmail.com>
2019-12-09 14:18:19 +0900

    알림장, 게시판 화면 생성

--- app.js
+++ app.js
@@ -42,11 +42,13 @@ connection.connect((err) => {
 });
 //-----------DB------------------
 
+// 추후 수정
+// 로그인 되어있으면 -> home
+// 로그인 안돼있으면 -> login
 app.get('/', (req, res) => {
-	
     res.render('home');
-    
 });
+
 app.get('/home', (req, res) =>{
     res.render('home')
 });
@@ -55,6 +57,13 @@ app.get('/login', (req, res) =>{
     res.render('login')
 });
 
+app.get('/notice', (req, res) =>{
+    res.render('notice')
+});
+
+app.get('/board', (req, res) =>{
+    res.render('board')
+});
 
 app.listen(8888, () => {
 	console.log('8888 port opened!!!');
