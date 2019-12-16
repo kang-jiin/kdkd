5a6a92c396b5b6fccdeafa051a850eb95db92b4d
kangG13 <dmssk13@gmail.com>
2019-12-16 17:54:26 +0900

    home 반내부 카메라 기능 구현

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(session({
 }));
 
 app.use(function (req, res, next) {
-    // req.session.userid = '1';
-    // req.session.name = '1';
-    // req.session.grade = 'G';
+    req.session.userid = '1';
+    req.session.name = '1';
+    req.session.grade = 'G';
     res.locals.user = req.session;
     res.locals.menu = req.url.split('/')[1];
     if(!req.session.userid && res.locals.menu != 'login') {
@@ -49,6 +49,32 @@ http.listen(8888, () => {
     console.log('8888 port opened!!!');
 })
 //-----------DB------------------
+
+//--------------Web Cam---------------
+const path = require('path');
+const WebSocket = require('ws');
+const wsServer = new WebSocket.Server({ server: http }, () => console.log(`WS server is listening at ws://localhost:${WS_PORT}`));
+
+// array of connected websocket clients
+let connectedClients = [];
+
+wsServer.on('connection', (ws, req) => {
+    console.log('Connected');
+    // add new connected client
+    connectedClients.push(ws);
+    // listen for messages from the streamer, the clients will not send anything so we don't need to filter
+    ws.on('message', data => {
+        // send the base64 encoded frame to each connected ws
+        connectedClients.forEach((ws, i) => {
+            if (ws.readyState === ws.OPEN) { // check if it is still connected
+                ws.send(data); // send
+            } else { // if it's not connected remove from the array of connected ws
+                connectedClients.splice(i, 1);
+            }
+        });
+    });
+});
+//--------------Web Cam---------------
 
 app.get('/', (req, res) => {
     res.redirect('/home');
@@ -436,6 +462,8 @@ app.get('/home', (req, res) => {
         });
     });
 });
+
+app.get('/streamer', (req, res) => res.sendFile(path.resolve(__dirname, './views/streamer.html')));
 
 //////////////////////////////////////////////////////////////
 //                      알림장                              //
