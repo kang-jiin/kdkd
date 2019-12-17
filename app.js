8d1f2a8f96bc0080ee811997ea06c94fc29437e4
kangG13 <dmssk13@gmail.com>
2019-12-17 16:46:06 +0900

    카메라 socket io로 변경

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(session({
 }));
 
 app.use(function (req, res, next) {
-    req.session.userid = '1';
-    req.session.name = '1';
-    req.session.grade = 'G';
+    req.session.userid = 'admin';
+    req.session.name = '관리자';
+    req.session.grade = 'A';
     res.locals.user = req.session;
     res.locals.menu = req.url.split('/')[1];
     if(!req.session.userid && res.locals.menu != 'user') {
@@ -50,28 +50,13 @@ http.listen(8888, () => {
 
 //--------------Web Cam---------------
 const path = require('path');
-const WebSocket = require('ws');
-const wsServer = new WebSocket.Server({ server: http }, () => console.log(`WS server is listening at ws://localhost:${WS_PORT}`));
-
-// array of connected websocket clients
-let connectedClients = [];
-
-wsServer.on('connection', (ws, req) => {
-    console.log('Connected');
-    // add new connected client
-    connectedClients.push(ws);
-    // listen for messages from the streamer, the clients will not send anything so we don't need to filter
-    ws.on('message', data => {
-        // send the base64 encoded frame to each connected ws
-        connectedClients.forEach((ws, i) => {
-            if (ws.readyState === ws.OPEN) { // check if it is still connected
-                ws.send(data); // send
-            } else { // if it's not connected remove from the array of connected ws
-                connectedClients.splice(i, 1);
-            }
-        });
+var io = require("socket.io")(http);
+io.on('connection',function(socket){
+    socket.on('stream',function(image){
+      socket.broadcast.emit('stream',image);
     });
-});
+  });
+  
 //--------------Web Cam---------------
 
 app.use('/user', require('./routes/user.js'));
