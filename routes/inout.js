d0eeb2ca441bebfbbd16115d9b9476a4f985c0f9
kangG13 <dmssk13@gmail.com>
2019-12-17 12:23:13 +0900

    등하원 - 관리자 계정일때 모든 학생 다 보이게 수정

--- routes/inout.js
+++ routes/inout.js
@@ -18,33 +18,37 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     const sess = req.session;
     let id = sess.userid;
+    let grade = sess.grade;
 
-    let in_select = `
-        select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no
-        from relation, student, in_out
-        where relation.student_id = student.id 
-        and student.id = in_out.student_id 
-        and in_out.in_out_flag = 'in' 
-        and relation.parents_id = ?
-        order by in_out.time desc
-    `;
-    let out_select = `
-        select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no
-        from relation, student, in_out
-        where relation.student_id = student.id 
-        and student.id = in_out.student_id 
-        and in_out.in_out_flag = 'out' 
-        and relation.parents_id = ?
-        order by in_out.time desc
-    `;
+    let inout_select;
+    if(grade == 'G') {  //일반 계정
+        inout_select = `
+            select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no
+            from relation, student, in_out
+            where relation.student_id = student.id 
+            and student.id = in_out.student_id 
+            and in_out.in_out_flag = ? 
+            and relation.parents_id = ?
+            order by in_out.time desc
+        `;
+    }
+    else if(grade == 'A') { //관리자 계정
+        inout_select = `
+            select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no
+            from student, in_out
+            where student.id = in_out.student_id 
+            and in_out.in_out_flag = ?
+            order by in_out.time desc
+        `;
+    }
     pool.getConnection((err, connection) => {
-        connection.query(in_select, id, (err, result1) =>{
+        connection.query(inout_select, ['in', id], (err, result1) =>{
             if (err) {
                 console.log(err);
                 connection.release();
                 res.status(500).send('Internal Server Error!!!')
             }
-            connection.query(out_select, id, (err, result2) =>{
+            connection.query(inout_select, ['out',id], (err, result2) =>{
                 if (err) {
                     console.log(err);
                     connection.release();
