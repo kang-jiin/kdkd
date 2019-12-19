c5cf106f6433ad5f30f06561b916b0f9fc0d11e5
dongmin7322 <dongmin7322@naver.com>
2019-12-19 11:00:22 +0900

    in out 유치원 버스 구분 완료

--- routes/inout.js
+++ routes/inout.js
@@ -31,7 +31,7 @@ router.get('/', (req, res) => {
     let inout_select;
     if(grade == 'P') {  //일반 계정
         inout_select = `
-            select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no
+            select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no, in_out.code as code
             from relation, student, in_out
             where relation.student_id = student.id 
             and student.id = in_out.student_id 
@@ -43,7 +43,7 @@ router.get('/', (req, res) => {
     }
     else if(grade == 'A') { //관리자 계정
         inout_select = `
-            select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no
+            select date_format(in_out.time, '%Y-%m-%d %H:%i:%s') as time, student.name as name, in_out.id as no, in_out.code as code
             from student, in_out
             where student.id = in_out.student_id 
             and date_format(in_out.time, '%Y-%m') = ?
